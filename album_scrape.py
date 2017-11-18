31d2f9497689b9605d1c3e31000de16a9a2e839a
Ryan <rc3705@hotmail.com>
2017-11-18 19:32:17 +1100

    Fixed navbar file paths

--- album_scrape.py
+++ album_scrape.py
@@ -21,7 +21,7 @@ for i in range(len(dirnames)):
 	f.close()
 	replace_title = filedata.replace("#album_title",dirnames[i])
 	# remove spaces from the folder name before saving file
-	f = open('%s\%s.html' %(dirnames[i],dirnames[i].replace(" ","")),'w')
+	f = open('%s.html' %(dirnames[i].replace(" ","")),'w')
 	f.write(replace_title)
 	f.close()
 
@@ -44,7 +44,7 @@ for i in range(len(dirnames)):
 		# print("%s/%s/*.jpg" %(dirnames[i],dirnames_temp[j]))
 		# print(img_list)
 		# append a tag for each subfolder
-		card_tag+=str('<div class="card">\n<img class="card-img-top card" src="%s" alt="Card image cap">\n<div class="card-body">\n<h4 class="card-title">%s</h4>\n<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content. </p>\n <a href="%s.html" class="btn btn-primary">View Album</a>\n</div>\n</div>'%(img_list[random.randint(0,len(img_list)-1)],dirnames_temp[j],dirnames_temp[j].replace(" ","")))
+		card_tag+=str('<div class="card">\n<img class="card-img-top card" src="%s" alt="Card image cap">\n<div class="card-body">\n<h4 class="card-title">%s</h4>\n<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content. </p>\n <a href="%s\%s.html" class="btn btn-primary">View Album</a>\n</div>\n</div>'%(img_list[random.randint(0,len(img_list)-1)],dirnames_temp[j],dirnames[i],dirnames_temp[j].replace(" ","")))
 
 		# init the image_tag var and make the tags
 		image_tag=str("")
@@ -73,19 +73,19 @@ for i in range(len(dirnames)):
 	card_tag+=str('\n\n#temp_tag')
 
 	# open the respective file and append the new card tags to the file
-	f = open('%s\%s.html' %(dirnames[i],dirnames[i].replace(" ","")),'r')
+	f = open('%s.html' %(dirnames[i].replace(" ","")),'r')
 	filedata = f.read()
 	f.close()
 	replace_card = filedata.replace("<!-- #card_tag_here -->",card_tag)
-	f = open('%s\%s.html' %(dirnames[i],dirnames[i].replace(" ","")),'w')
+	f = open('%s.html' %(dirnames[i].replace(" ","")),'w')
 	f.write(replace_card)
 	f.close()
 
 	#replace temp tag so new cards can be appended later
-	f = open('%s\%s.html' %(dirnames[i],dirnames[i].replace(" ","")),'r')
+	f = open('%s.html' %(dirnames[i].replace(" ","")),'r')
 	filedata = f.read()
 	f.close()
 	replace_tag = filedata.replace("#temp_tag","<!-- #card_tag_here -->")
-	f = open('%s\%s.html' %(dirnames[i],dirnames[i].replace(" ","")),'w')
+	f = open('%s.html' %(dirnames[i].replace(" ","")),'w')
 	f.write(replace_tag)
 	f.close()
