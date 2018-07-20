28b181c39ea351fea0b1083abb4728da7c06b3d0
Ryan <rc3705@hotmail.com>
2018-07-20 19:08:46 +0200

    Image resizing tweaks

--- image_resize.py
+++ image_resize.py
@@ -1,10 +1,13 @@
 from PIL import Image
 
-# basewidth=[300,600,900,1200]
-
+basewidth=[300,600,900,1200]
+file_name="Europe/Antwerp/zcover"
+#img.save('%s_%s.jpg' %(file_name,int(img.width)))
 for i in range (len(basewidth)):
-	img = Image.open(file_name)
-	wpercent = (basewidth[i]/float(img.size[0]))
-	hsize = int((float(img.size[1])*float(wpercent)))
-	img = img.resize((basewidth[i],hsize), Image.ANTIALIAS)
-	img.save('%s_%s.jpg' %(file_name,basewidth[i])) 
+	img = Image.open('%s.jpg' %file_name)
+	wpercent = (basewidth[i]/float(img.width))
+	hsize = int((float(img.height)*float(wpercent)))
+	img = img.resize((basewidth[i],hsize), Image.LANCZOS)
+	img.save('%s_%s.jpg' %(file_name,basewidth[i]),'jpeg',icc_profile=img.info.get('icc_profile'))
+
+img.close()
